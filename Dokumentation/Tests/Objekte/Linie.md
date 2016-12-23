55ea1a95a42cdd6b4ba7ce9470cba980ca69a8f9
André <github@andrekirst.de>
2016-12-23 11:26:45 +0100

    #14 - Erstellung Objekt, Test und Dokumentation

---
+++ Dokumentation/Tests/Objekte/Linie.md
@@ -0,0 +1,9 @@
+# Objekt *Linie*
+
+## Testfälle
+
+### Linie_Konstruktor_Name_Test_Ident_TEST
+
+#### Beschreibung:
+
+Test, ob der Name den Wert "Test" beinhaltet und der Ident den Wert "TEST" beinhaltet.
---
+++ src/Fahrplanauskunft/Fahrplanauskunft.Test/T_Linie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Fahrplanauskunft.Objekte;
+
+namespace Fahrplanauskunft.Test
+{
+    [TestClass]
+    public class T_Linie
+    {
+        /// <summary>
+        /// Test, ob der Name den Wert "Test" beinhaltet und der Ident den Wert "TEST" beinhaltet.
+        /// </summary>
+        [TestMethod, TestCategory("Objekte")]
+        public void Linie_Konstruktor_Name_Test_Ident_TEST()
+        {
+            string name = "Test";
+            string ident = "TEST";
+            Linie linie = new Linie(name, ident);
+
+            Assert.AreEqual("Test", linie.Name);
+            Assert.AreEqual("TEST", linie.Ident);
+        }
+    }
+}
