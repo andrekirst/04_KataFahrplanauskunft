db0b71c5b24229f8abbf20452adc1c6ce9f9b41f
andrekirst <github@andrekirst.de>
2016-12-25 18:52:50 +0100

    #19 - Objekt und Test implementiert

---
+++ src/Fahrplanauskunft/Fahrplanauskunft.Test/Objekte/T_HaltestelleFahrplanEintrag.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Fahrplanauskunft.Objekte;
+
+namespace Fahrplanauskunft.Test.Objekte
+{
+    /// <summary>
+    /// Summary description for T_HaltestelleFahrplanEintrag
+    /// </summary>
+    [TestClass]
+    public class T_HaltestelleFahrplanEintrag
+    {
+        /// <summary>
+        /// Test, ob die Haltestelle nicht NULL ist, dass die Uhrzeit den Wert "750" hat und die Linie nicht NULL ist.
+        /// </summary>
+        [TestMethod]
+        public void Konstruktor_Haltestelle_NICHT_NULL_Uhrzeit_750_Linie_NICHT_NULL()
+        {
+            Haltestelle haltestelle = new Haltestelle(name: "Haltestelle1");
+            Linie linie = new Linie(name: "U1", ident: "U1_NORD");
+
+            HaltestelleFahrplanEintrag haltestelleFahrplanEintrag = new HaltestelleFahrplanEintrag(haltestelle: haltestelle, uhrzeit: 750, linie: linie);
+
+            Assert.IsNotNull(haltestelleFahrplanEintrag.Haltestelle);
+            Assert.AreEqual(750, haltestelleFahrplanEintrag.Uhrzeit);
+            Assert.IsNotNull(haltestelleFahrplanEintrag.Linie);
+        }
+    }
+}
---
+++ src/Fahrplanauskunft/Fahrplanauskunft/Objekte/HaltestelleFahrplanEintrag.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fahrplanauskunft.Objekte
+{
+    /// <summary>
+    /// Ein HaltestelleFahrplanEintrag beinhaltet die Information, wann, wo und mit welche Linie abfährt
+    /// </summary>
+    public class HaltestelleFahrplanEintrag
+    {
+        /// <summary>
+        /// Standardkonstruktor
+        /// </summary>
+        public HaltestelleFahrplanEintrag()
+        {
+        }
+
+        /// <summary>
+        /// Konstruktor, um die Haltestelle, Uhrzeit und die Linie zu setzen
+        /// </summary>
+        /// <param name="haltestelle">Eine Haltestelle</param>
+        /// <param name="uhrzeit">Die Uhrzeit, wann abgefahren wird</param>
+        /// <param name="linie">Die Linie, die an der Haltestelle los fährt</param>
+        public HaltestelleFahrplanEintrag(Haltestelle haltestelle, int uhrzeit, Linie linie)
+        {
+            Haltestelle = haltestelle;
+            Uhrzeit = uhrzeit;
+            Linie = linie;
+        }
+
+        /// <summary>
+        /// Gibt die Haltestelle zurück, welche die Linie abfährt
+        /// </summary>
+        public object Haltestelle
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Die Uhrzeit, wann abgefahren wird
+        /// </summary>
+        public object Linie
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Die Linie, die an der Haltestelle fährt
+        /// </summary>
+        public int Uhrzeit
+        {
+            get;
+            set;
+        }
+    }
+}
