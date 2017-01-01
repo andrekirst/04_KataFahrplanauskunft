1e7110e34fc96d3585e6a1bb9424289452a5251a
andrekirst <github@andrekirst.de>
2017-01-01 20:18:46 +0100

    #28 - Erstellung von Objekt- und Test-Klasse

---
+++ src/Fahrplanauskunft/Fahrplanauskunft.Test/Objekte/T_FahrplanauskunftSpeicher.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Fahrplanauskunft.Objekte;
+using System.Collections.Generic;
+
+namespace Fahrplanauskunft.Test.Objekte
+{
+    /// <summary>
+    /// Test-Klasse für das Objekt FahrplanauskunftSpeicher
+    /// </summary>
+    [TestClass]
+    public class T_FahrplanauskunftSpeicher
+    {
+        /// <summary>
+        /// Test, ob der FahrplanauskunftSpeicher die Dateien im Ordner lädt
+        /// </summary>
+        [TestMethod, TestCategory("Objekte")]
+        public void FahrplanauskunftSpeicher_Konstruktor_OrdnerPfad()
+        {
+            string ordnerPfad = "./TestData/TestData1";
+
+            FahrplanauskunftSpeicher fahrplanauskunftSpeicher = new FahrplanauskunftSpeicher(ordnerPfad: ordnerPfad);
+
+            Assert.AreEqual("./TestData/TestData1", fahrplanauskunftSpeicher.OrdnerPfad);
+        }
+    }
+}
---
+++ src/Fahrplanauskunft/Fahrplanauskunft/Objekte/FahrplanauskunftSpeicher.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fahrplanauskunft.Objekte
+{
+    public class FahrplanauskunftSpeicher
+    {
+        /// <summary>
+        /// Standardkonstruktor
+        /// </summary>
+        public FahrplanauskunftSpeicher()
+        {
+        }
+
+        /// <summary>
+        /// Konstruktor mit dem Ordner-Pfad, in dem sich die Fahrplanauskunfts-Dateien befinden
+        /// </summary>
+        /// <param name="ordnerPfad">Der Pfad, in dem sich die Fahrplanauskunfts-Dateien befinden</param>
+        public FahrplanauskunftSpeicher(string ordnerPfad)
+        {
+            OrdnerPfad = ordnerPfad;
+        }
+
+        /// <summary>
+        /// Gibt oder setzt den Pfad, in dem sich die Fahrplanauskunfts-Dateien befinden
+        /// </summary>
+        public string OrdnerPfad
+        {
+            get;
+            set;
+        }
+    }
+}
