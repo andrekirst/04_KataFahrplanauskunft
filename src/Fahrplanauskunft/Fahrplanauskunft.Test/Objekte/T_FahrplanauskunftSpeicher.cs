6216fa92b9dbf94f6c72905827285f353e549e60
andrekirst <github@andrekirst.de>
2017-01-15 16:56:33 +0100

    #36 - Test erstellt

--- src/Fahrplanauskunft/Fahrplanauskunft.Test/Objekte/T_FahrplanauskunftSpeicher.cs
+++ src/Fahrplanauskunft/Fahrplanauskunft.Test/Objekte/T_FahrplanauskunftSpeicher.cs
@@ -179,7 +179,63 @@ namespace Fahrplanauskunft.Test.Objekte
                 new Streckenabschnitt(1, haltestellen[0], haltestellen[1], linien)
             };
 
-        #endregion
+            #endregion
+
+            List<Streckenabschnitt> actual = fahrplanauskunftSpeicher.Streckenabschnitte;
+
+            List<Streckenabschnitt> expected = streckenabschnitte;
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// Test, dass Streckenabschnitte geladen werden und 10 Streckenabschnitte als Quelle und im Ziel vorhanden sind
+        /// </summary>
+        [TestMethod, TestCategory("Objekte")]
+        public void FahrplanauskunftSpeicher_LadeStreckenabschnitte_10()
+        {
+            string ordnerPfad = "TestDaten\\TestSatz6";
+
+            FahrplanauskunftSpeicher fahrplanauskunftSpeicher = new FahrplanauskunftSpeicher(ordnerPfad: ordnerPfad);
+            fahrplanauskunftSpeicher.LadeStreckenabschnitte();
+
+            #region Erstellung des zu erwartendem Wertes
+            List<Linie> linien = new List<Linie>()
+            {
+                new Linie(name: "U1", ident: "U1_NORD"),
+                new Linie(name: "U1", ident: "U1_SUED")
+            };
+
+            List<Haltestelle> haltestellen = new List<Haltestelle>()
+            {
+                new Haltestelle(name: "H1") { Linien = linien },
+                new Haltestelle(name: "H2") { Linien = linien },
+                new Haltestelle(name: "H3") { Linien = linien },
+                new Haltestelle(name: "H4") { Linien = linien },
+                new Haltestelle(name: "H5") { Linien = linien },
+                new Haltestelle(name: "H6") { Linien = linien },
+                new Haltestelle(name: "H7") { Linien = linien },
+                new Haltestelle(name: "H8") { Linien = linien },
+                new Haltestelle(name: "H9") { Linien = linien },
+                new Haltestelle(name: "H10") { Linien = linien },
+                new Haltestelle(name: "H11") { Linien = linien }
+            };
+
+            List<Streckenabschnitt> streckenabschnitte = new List<Streckenabschnitt>()
+            {
+                new Streckenabschnitt(1, haltestellen[0], haltestellen[1], linien),
+                new Streckenabschnitt(1, haltestellen[1], haltestellen[2], linien),
+                new Streckenabschnitt(1, haltestellen[2], haltestellen[3], linien),
+                new Streckenabschnitt(1, haltestellen[3], haltestellen[4], linien),
+                new Streckenabschnitt(1, haltestellen[4], haltestellen[5], linien),
+                new Streckenabschnitt(1, haltestellen[5], haltestellen[6], linien),
+                new Streckenabschnitt(1, haltestellen[6], haltestellen[7], linien),
+                new Streckenabschnitt(1, haltestellen[7], haltestellen[8], linien),
+                new Streckenabschnitt(1, haltestellen[8], haltestellen[9], linien),
+                new Streckenabschnitt(1, haltestellen[9], haltestellen[10], linien)
+            };
+
+            #endregion
 
             List<Streckenabschnitt> actual = fahrplanauskunftSpeicher.Streckenabschnitte;
 
