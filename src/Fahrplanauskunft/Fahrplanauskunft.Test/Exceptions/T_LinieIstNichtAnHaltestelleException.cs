89b4cedac5eb1f28c9f5c3dc09d5e7654b7bf2a7
andrekirst <github@andrekirst.de>
2017-05-08 21:34:32 +0200

    #80 - Neue Testklasse für Exception

---
+++ src/Fahrplanauskunft/Fahrplanauskunft.Test/Exceptions/T_LinieIstNichtAnHaltestelleException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fahrplanauskunft.Test.Exceptions
+{
+    /// <summary>
+    /// Testklasse für LinieIstNichtAnHaltestelleException
+    /// </summary>
+    [TestClass]
+    public class T_LinieIstNichtAnHaltestelleException
+    {
+        /// <summary>
+        /// Test für die Eigenschaft Linie
+        /// </summary>
+        [TestMethod, TestCategory("Exceptions")]
+        public void LinieIstNichtAnHaltestelleException_Property_Linie()
+        {
+            Assert.Fail();
+        }
+
+        /// <summary>
+        /// Test für die Eigenschaft Haltestelle
+        /// </summary>
+        [TestMethod, TestCategory("Exceptions")]
+        public void LinieIstNichtAnHaltestelleException_Property_Haltestelle()
+        {
+            Assert.Fail();
+        }
+    }
+}
