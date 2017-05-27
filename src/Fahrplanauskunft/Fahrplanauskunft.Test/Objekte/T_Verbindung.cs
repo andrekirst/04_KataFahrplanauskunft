9c33d9ad2fb63e2c9eb2e5b1c20d60211f82d41f
andrekirst <github@andrekirst.de>
2017-05-27 12:16:39 +0200

    #97 - Implementierung Verbindung

--- src/Fahrplanauskunft/Fahrplanauskunft.Test/Objekte/T_Verbindung.cs
+++ src/Fahrplanauskunft/Fahrplanauskunft.Test/Objekte/T_Verbindung.cs
@@ -28,31 +28,154 @@ namespace Fahrplanauskunft.Test.Objekte
         [TestMethod, TestCategory("Objekte")]
         public void Verbindung_Property_StartHaltestelle()
         {
-            Assert.Fail();
+            Verbindung verbindung = TestVerbindung();
+
+            Haltestelle expected = new Haltestelle(name: "H1");
+
+            Assert.AreEqual(expected, verbindung.StartHaltestelle);
         }
 
         [TestMethod, TestCategory("Objekte")]
         public void Verbindung_Property_ZielHaltestelle()
         {
-            Assert.Fail();
+            Verbindung verbindung = TestVerbindung();
+
+            Haltestelle expected = new Haltestelle(name: "H2");
+
+            Assert.AreEqual(expected, verbindung.ZielHaltestelle);
         }
 
         [TestMethod, TestCategory("Objekte")]
         public void Verbindung_Property_Einzelverbindungen()
         {
-            Assert.Fail();
+            Verbindung verbindung = TestVerbindung();
+
+            Dictionary<int, Einzelverbindung> expected = new Dictionary<int, Einzelverbindung>();
+            Einzelverbindung einzelverbindung = new Einzelverbindung(
+                abfahrtszeit: 720,
+                ankunftszeit: 770,
+                startHaltestelle: new Haltestelle(name: "H1"),
+                zielHaltestelle: new Haltestelle(name: "H2"),
+                linie: new Linie(name: "Linie 1", ident: "L1"));
+
+            expected.Add(1, einzelverbindung);
+
+            Dictionary<int, Einzelverbindung> actual = verbindung.Einzelverbindungen;
+
+            CollectionAssert.AreEqual(expected, actual);
         }
 
         [TestMethod, TestCategory("Objekte")]
         public void Verbindung_Equals()
         {
-            Assert.Fail();
+            Dictionary<int, Einzelverbindung> einzelverbindungen1 = new Dictionary<int, Einzelverbindung>();
+            Einzelverbindung einzelverbindung1 = new Einzelverbindung(
+                abfahrtszeit: 720,
+                ankunftszeit: 770,
+                startHaltestelle: new Haltestelle(name: "H1"),
+                zielHaltestelle: new Haltestelle(name: "H2"),
+                linie: new Linie(name: "Linie 1", ident: "L1"));
+
+            einzelverbindungen1.Add(1, einzelverbindung1);
+
+            Verbindung verbindung1 = new Verbindung(
+                abfahrtszeit: 720,
+                ankunftszeit: 770,
+                startHaltestelle: new Haltestelle(name: "H1"),
+                zielHaltestelle: new Haltestelle(name: "H2"),
+                einzelverbindungen: einzelverbindungen1);
+
+            Dictionary<int, Einzelverbindung> einzelverbindungen2 = new Dictionary<int, Einzelverbindung>();
+            Einzelverbindung einzelverbindung2 = new Einzelverbindung(
+                abfahrtszeit: 720,
+                ankunftszeit: 770,
+                startHaltestelle: new Haltestelle(name: "H1"),
+                zielHaltestelle: new Haltestelle(name: "H2"),
+                linie: new Linie(name: "Linie 1", ident: "L1"));
+
+            einzelverbindungen2.Add(1, einzelverbindung2);
+
+            Verbindung verbindung2 = new Verbindung(
+                abfahrtszeit: 720,
+                ankunftszeit: 770,
+                startHaltestelle: new Haltestelle(name: "H1"),
+                zielHaltestelle: new Haltestelle(name: "H2"),
+                einzelverbindungen: einzelverbindungen2);
+
+            Assert.AreEqual(verbindung1, verbindung2);
+        }
+
+        [TestMethod, TestCategory("Objekte")]
+        public void Verbindung_NotEquals()
+        {
+            Dictionary<int, Einzelverbindung> einzelverbindungen1 = new Dictionary<int, Einzelverbindung>();
+            Einzelverbindung einzelverbindung1 = new Einzelverbindung(
+                abfahrtszeit: 720,
+                ankunftszeit: 770,
+                startHaltestelle: new Haltestelle(name: "H1"),
+                zielHaltestelle: new Haltestelle(name: "H2"),
+                linie: new Linie(name: "Linie 1", ident: "L1"));
+
+            einzelverbindungen1.Add(1, einzelverbindung1);
+
+            Verbindung verbindung1 = new Verbindung(
+                abfahrtszeit: 720,
+                ankunftszeit: 770,
+                startHaltestelle: new Haltestelle(name: "H1"),
+                zielHaltestelle: new Haltestelle(name: "H2"),
+                einzelverbindungen: einzelverbindungen1);
+
+            Dictionary<int, Einzelverbindung> einzelverbindungen2 = new Dictionary<int, Einzelverbindung>();
+            Einzelverbindung einzelverbindung2 = new Einzelverbindung(
+                abfahrtszeit: 720,
+                ankunftszeit: 770,
+                startHaltestelle: new Haltestelle(name: "H1"),
+                zielHaltestelle: new Haltestelle(name: "H2"),
+                linie: new Linie(name: "Linie 2", ident: "L2"));
+
+            einzelverbindungen2.Add(1, einzelverbindung2);
+
+            Verbindung verbindung2 = new Verbindung(
+                abfahrtszeit: 720,
+                ankunftszeit: 770,
+                startHaltestelle: new Haltestelle(name: "H1"),
+                zielHaltestelle: new Haltestelle(name: "H2"),
+                einzelverbindungen: einzelverbindungen2);
+
+            Assert.AreNotEqual(verbindung1, verbindung2);
         }
 
         [TestMethod, TestCategory("Objekte")]
         public void Verbindung_ToString()
         {
-            Assert.Fail();
+            Verbindung verbindung = TestVerbindung();
+
+            string expected = "H1 12:00 -> H2 12:50 - Umstiege: 0";
+            string actual = verbindung.ToString();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, TestCategory("Objekte")]
+        public void Verbindung_AnzahlUmstiege_0()
+        {
+            Verbindung verbindung = TestVerbindung();
+
+            int expected = 0;
+            int actual = verbindung.AnzahlUmstiege;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod, TestCategory("Objekte")]
+        public void Verbindung_AnzahlUmstiege_1()
+        {
+            Verbindung verbindung = TestVerbindung_2_Einzelverbindugen();
+
+            int expected = 1;
+            int actual = verbindung.AnzahlUmstiege;
+
+            Assert.AreEqual(expected, actual);
         }
 
         private Verbindung TestVerbindung()
@@ -72,7 +195,37 @@ namespace Fahrplanauskunft.Test.Objekte
                 ankunftszeit: 770,
                 startHaltestelle: new Haltestelle(name: "H1"),
                 zielHaltestelle: new Haltestelle(name: "H2"),
-                einzelerverbindungen: einzelverbindungen);
+                einzelverbindungen: einzelverbindungen);
+            return verbindung;
+        }
+
+        private Verbindung TestVerbindung_2_Einzelverbindugen()
+        {
+            Dictionary<int, Einzelverbindung> einzelverbindungen = new Dictionary<int, Einzelverbindung>();
+            Einzelverbindung einzelverbindung = new Einzelverbindung(
+                abfahrtszeit: 720,
+                ankunftszeit: 770,
+                startHaltestelle: new Haltestelle(name: "H1"),
+                zielHaltestelle: new Haltestelle(name: "H2"),
+                linie: new Linie(name: "Linie 1", ident: "L1"));
+
+            einzelverbindungen.Add(1, einzelverbindung);
+
+            Einzelverbindung einzelverbindung2 = new Einzelverbindung(
+                abfahrtszeit: 780,
+                ankunftszeit: 810,
+                startHaltestelle: new Haltestelle(name: "H2"),
+                zielHaltestelle: new Haltestelle(name: "H3"),
+                linie: new Linie(name: "Linie 2", ident: "L2"));
+
+            einzelverbindungen.Add(2, einzelverbindung2);
+
+            Verbindung verbindung = new Verbindung(
+                abfahrtszeit: 720,
+                ankunftszeit: 770,
+                startHaltestelle: new Haltestelle(name: "H1"),
+                zielHaltestelle: new Haltestelle(name: "H2"),
+                einzelverbindungen: einzelverbindungen);
             return verbindung;
         }
     }
--- src/Fahrplanauskunft/Fahrplanauskunft/Objekte/Verbindung.cs
+++ src/Fahrplanauskunft/Fahrplanauskunft/Objekte/Verbindung.cs
@@ -2,6 +2,7 @@
  Copyright: André Kirst
  Datei: Verbindung.cs
 */
+using Fahrplanauskunft.Funktionen;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,18 +13,13 @@ namespace Fahrplanauskunft.Objekte
 {
     public class Verbindung
     {
-        private int ankunftszeit;
-        private Dictionary<int, Einzelverbindung> einzelerverbindungen;
-        private Haltestelle startHaltestelle;
-        private Haltestelle zielHaltestelle;
-
-        public Verbindung(int abfahrtszeit, int ankunftszeit, Haltestelle startHaltestelle, Haltestelle zielHaltestelle, Dictionary<int, Einzelverbindung> einzelerverbindungen)
+        public Verbindung(int abfahrtszeit, int ankunftszeit, Haltestelle startHaltestelle, Haltestelle zielHaltestelle, Dictionary<int, Einzelverbindung> einzelverbindungen)
         {
             Abfahrtszeit = abfahrtszeit;
             Ankunftszeit = ankunftszeit;
-            this.startHaltestelle = startHaltestelle;
-            this.zielHaltestelle = zielHaltestelle;
-            this.einzelerverbindungen = einzelerverbindungen;
+            StartHaltestelle = startHaltestelle;
+            ZielHaltestelle = zielHaltestelle;
+            Einzelverbindungen = einzelverbindungen;
         }
 
         public int Abfahrtszeit
@@ -31,10 +27,68 @@ namespace Fahrplanauskunft.Objekte
             get;
             set;
         }
+
         public int Ankunftszeit
         {
             get;
             set;
+        }
+
+        public int AnzahlUmstiege
+        {
+            get
+            {
+                return Einzelverbindungen.Count - 1;
+            }
+        }
+
+        public Dictionary<int, Einzelverbindung> Einzelverbindungen
+        {
+            get;
+            set;
+        }
+
+        public Haltestelle StartHaltestelle
+        {
+            get;
+            set;
+        }
+
+        public Haltestelle ZielHaltestelle
+        {
+            get;
+            set;
+        }
+
+        public override string ToString()
+        {
+            return String.Format(
+                "{0} {1} -> {2} {3} - Umstiege: {4}",
+                StartHaltestelle.Name,
+                Funktionen.ZeitKonverter.ZuUhrzeitText(Abfahrtszeit),
+                ZielHaltestelle.Name,
+                Funktionen.ZeitKonverter.ZuUhrzeitText(Ankunftszeit),
+                AnzahlUmstiege);
+        }
+
+        public bool Equals(Verbindung other)
+        {
+            return EqualsHelper.EqualBase<Verbindung>(
+                other,
+                () =>
+                {
+                    return
+                        this.Abfahrtszeit == other.Abfahrtszeit &&
+                        this.Ankunftszeit == other.Ankunftszeit &&
+                        this.StartHaltestelle == other.StartHaltestelle &&
+                        this.ZielHaltestelle == other.ZielHaltestelle &&
+                        this.Einzelverbindungen.SequenceEqual(other.Einzelverbindungen);
+                });
+        }
+
+        public override bool Equals(object obj)
+        {
+            return this.Equals(obj as Verbindung);
         }
     }
 }
