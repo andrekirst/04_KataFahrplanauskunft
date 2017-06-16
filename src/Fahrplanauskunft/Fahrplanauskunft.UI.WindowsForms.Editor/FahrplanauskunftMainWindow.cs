d9fb314b8d26bb765fdf40f1cc540fc6948a08f3
Andre Kirst <github@andrekirst.de>
2017-06-16 11:07:34 +0200

    #109 - Test und Implementierung für ResourceHelper

--- src/Fahrplanauskunft/Fahrplanauskunft.UI.WindowsForms.Editor/FahrplanauskunftMainWindow.cs
+++ src/Fahrplanauskunft/Fahrplanauskunft.UI.WindowsForms.Editor/FahrplanauskunftMainWindow.cs
@@ -1,15 +1,7 @@
-﻿// <copyright file="Form1.cs" company="github.com/andrekirst/04_KataFahrplanauskunft">
+﻿// <copyright file="FahrplanauskunftMainWindow.cs" company="github.com/andrekirst/04_KataFahrplanauskunft">
 // Copyright (c) github.com/andrekirst/04_KataFahrplanauskunft. All rights reserved.
 // </copyright>
 
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Fahrplanauskunft.UI.WindowsForms.Editor
---
+++ src/Fahrplanauskunft/Fahrplanauskunft.UI.WindowsForms.Editor/Helper/ResourceHelper.cs
@@ -0,0 +1,37 @@
+﻿// <copyright file="ResourceHelper.cs" company="github.com/andrekirst/04_KataFahrplanauskunft">
+// Copyright (c) github.com/andrekirst/04_KataFahrplanauskunft. All rights reserved.
+// </copyright>
+
+using System.Resources;
+
+namespace Fahrplanauskunft.UI.WindowsForms.Editor.Helper
+{
+    /// <summary>
+    /// Klasse für die Hilfsklasse ResourceHelper
+    /// </summary>
+    public class ResourceHelper
+    {
+        /// <summary>
+        /// Standardkonstruktor für die Klasse ResourceHelper
+        /// </summary>
+        public ResourceHelper()
+        {
+            ResourceManager = new ResourceManager("Fahrplanauskunft.UI.WindowsForms.Editor.Resources.Resources", typeof(FahrplanauskunftMainWindow).Assembly);
+        }
+
+        /// <summary>
+        /// Der Ressourcenmanager für die Hilfsklasse
+        /// </summary>
+        public ResourceManager ResourceManager { get; set; }
+
+        /// <summary>
+        /// Methode für das Laden eines Resourcenwertes anhand eines Ident
+        /// </summary>
+        /// <param name="resourceIdent">Der Identifizierer der Resource</param>
+        /// <returns>Gibt den Wert aus der Resourcendatei zurück</returns>
+        public string RessourcenTextFuerIdent(string resourceIdent)
+        {
+            return ResourceManager.GetString(resourceIdent);
+        }
+    }
+}
