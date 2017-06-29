ca56a26879b168dfa6cc9b3f1118a0021a754dd2
Andre Kirst <github@andrekirst.de>
2017-06-29 16:29:50 +0200

    #109 - Farben

--- src/Fahrplanauskunft/Fahrplanauskunft.UI.Windows.Editor/FahrplanauskunftMainWindow.xaml.cs
+++ src/Fahrplanauskunft/Fahrplanauskunft.UI.Windows.Editor/FahrplanauskunftMainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Threading;
 using System.Windows;
 using Fahrplanauskunft.Objekte;
+using System.Text.RegularExpressions;
 
 namespace Fahrplanauskunft.UI.Windows.Editor
 {
@@ -31,18 +32,32 @@ namespace Fahrplanauskunft.UI.Windows.Editor
 
         private void PrototypTest()
         {
+            string[] colors = new string[] { "#FF4500", "#FFDAB9", "#66CDAA", "#FFD700", "#6B8E23", "#32CD32" };
+
             List<Linie> linien = new List<Linie>();
             linien.Add(new Linie(name: "B1", ident: "B11"));
             linien.Add(new Linie(name: "B1", ident: "B12"));
+            linien.Add(new Linie(name: "B2", ident: "B21"));
+            linien.Add(new Linie(name: "B2", ident: "B22"));
+            linien.Add(new Linie(name: "B3", ident: "B31"));
+            linien.Add(new Linie(name: "B3", ident: "B32"));
+
+            linien.Add(new Linie(name: "B4", ident: "B41"));
+            linien.Add(new Linie(name: "B4", ident: "B42"));
+            linien.Add(new Linie(name: "B5", ident: "B51"));
+            linien.Add(new Linie(name: "B5", ident: "B52"));
+            linien.Add(new Linie(name: "B6", ident: "B61"));
+            linien.Add(new Linie(name: "B6", ident: "B62"));
 
             var items = from linie in linien
                         select new
                         {
                             Name = linie.Name,
                             Ident = linie.Ident,
-                            AnzahlHaltestellen = 20,
-                            AnzahlStreckenabschnitte = 20,
-                            AnzahlHaltestellenfahrplaneintraege = 1450
+                            AnzahlHaltestellen = new Random().Next(10, 20),
+                            AnzahlStreckenabschnitte = new Random().Next(10, 20),
+                            AnzahlHaltestellenfahrplaneintraege = new Random().Next(1000, 1500),
+                            Farbe = colors[int.Parse(new Regex("[^0-9 -]").Replace(linie.Name, string.Empty)) - 1]
                         };
 
             listBoxLinien.ItemsSource = items;
