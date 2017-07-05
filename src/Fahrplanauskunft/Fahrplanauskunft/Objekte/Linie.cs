e6553d30e48d04fbc01910dfd3abf45e2a829f2c
Andre Kirst <github@andrekirst.de>
2017-07-05 10:50:48 +0200

    #114 - Neuer Test, GetHashCode angepasst

--- src/Fahrplanauskunft/Fahrplanauskunft/Objekte/Linie.cs
+++ src/Fahrplanauskunft/Fahrplanauskunft/Objekte/Linie.cs
@@ -114,7 +114,10 @@ namespace Fahrplanauskunft.Objekte
         /// <returns>Der HashCode</returns>
         public override int GetHashCode()
         {
-            return (Name.GetHashCode() * 3) + (Ident.GetHashCode() * 2) + Farbe.GetHashCode();
+            return
+                (string.IsNullOrEmpty(Name) ? 0 : Name.GetHashCode() * 3) +
+                (string.IsNullOrEmpty(Ident) ? 0 : Ident.GetHashCode() * 2) +
+                (string.IsNullOrEmpty(Farbe) ? 0 : Farbe.GetHashCode());
         }
 
         /// <summary>
