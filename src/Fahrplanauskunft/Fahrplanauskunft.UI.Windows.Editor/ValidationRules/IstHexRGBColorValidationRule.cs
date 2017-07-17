650120d1e943a2943244e7e1244de7093e8c5800
Andre Kirst <github@andrekirst.de>
2017-07-17 12:55:55 +0200

    #109 - ValidationRule

---
+++ src/Fahrplanauskunft/Fahrplanauskunft.UI.Windows.Editor/ValidationRules/IstHexRGBColorValidationRule.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="IstHexRGBColorValidationRule.cs" company="github.com/andrekirst/04_KataFahrplanauskunft">
+// Copyright (c) github.com/andrekirst/04_KataFahrplanauskunft. All rights reserved.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Fahrplanauskunft.UI.Windows.Editor.ValidationRules
+{
+    public class IstHexRGBColorValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
